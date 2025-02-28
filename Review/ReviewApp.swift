3a89c2239ae31d74e2639ef0712b8853aecbc214
crazywolf132 <crazywolf132@gmail.com>
2025-02-28 16:06:53 +1100

    chore: first commit

---
+++ Review/ReviewApp.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewApp.swift
+//  Review
+//
+//  Created by Brayden Moon on 28/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReviewApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
